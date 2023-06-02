bc9e7556368055daf1dbdbb83201f805d9301264
Napster <rome.1205h@gmail.com>
2023-06-01 23:58:23 -0500

    add js advance

---
+++ script.js
@@ -0,0 +1,11 @@
+const boxes = document.querySelectorAll('.box');
+
+window.addEventListener('scroll', checkBoxes)
+
+checkBoxes();
+
+const checkBoxes = () => {
+   alert('hi')
+}
+
+console.log(boxes)
